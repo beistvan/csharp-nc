2e3bfd7cc7ae6e8b22c92f09d0977565bbe5dc5f
Beregszászi István <beistvan@gmail.com>
2023-06-06 07:01:05 +0200

    Echo

---
+++ Echo.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+
+        Console.WriteLine(text);
+        Console.WriteLine(text);
+    }
+}
