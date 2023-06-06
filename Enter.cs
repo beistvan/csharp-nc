2685ab35f7af5400f2ce67cdd02a428ad7912b9b
Beregszászi István <beistvan@gmail.com>
2023-06-06 07:01:35 +0200

    Enter From Console

---
+++ Enter.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+
+        string newLine = "You entered: " +  text;
+
+        Console.WriteLine(newLine);
+    }
+}
